d340076e499f0777d4795061e4ccd74154c9cd67
Jonathan Felipe Hernández Catalán <jfhernandez@ceroble.edu.gt>
2021-11-30 12:45:19 -0600

    Finalizando la clase ES9

--- src/es9/index.js
+++ src/es9/index.js
@@ -40,3 +40,17 @@ helloWorld()
     .then(response => console.log(response))
     .catch(error => console.log(error))
     .finally(() => console.log('Finalizó'))
+
+
+//RegExp
+
+const regExpDate = /([0-9]{4})-([0-9]{2})-([0-9]{2})/
+
+const match = regExpDate.exec('2021-11-30');
+
+const year = match[1];
+const month = match[2];
+const day = match[3]
+
+console.log(year, month, day);
+
